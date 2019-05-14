fac837094ab903e1215417456640ba8b86bbbcde
marek <marek@macbook-pro-marek.local>
2019-05-14 14:01:35 +0200

    Add simple design

--- Hotdog?/ViewController.swift
+++ Hotdog?/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
