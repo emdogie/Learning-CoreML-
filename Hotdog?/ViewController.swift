1e6112228b4f11ce1a2fe42fd591070e0f0c02ff
marek <marek@macbook-pro-marek.local>
2019-05-14 14:45:59 +0200

    App recognizes hotdog

--- Hotdog?/ViewController.swift
+++ Hotdog?/ViewController.swift
@@ -29,8 +29,43 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let imagePicked = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = imagePicked
+            
+            guard let ciiImage = CIImage(image: imagePicked) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            detect(image: ciiImage)
         }
         dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstresult = results.first {
+                if firstresult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog ☹️"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
